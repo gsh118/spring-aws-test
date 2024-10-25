40508e733be5ebef14ff65fc4672b494d1db9146
gsh118 <hks01108@naver.com>
2024-10-25 14:22:50 +0900

    feat: github action test

--- src/main/java/com/ohgiraffers/demoelasticbeanstalk/menu/entity/Menu.java
+++ src/main/java/com/ohgiraffers/demoelasticbeanstalk/menu/entity/Menu.java
@@ -1,4 +1,38 @@
 package com.ohgiraffers.demoelasticbeanstalk.menu.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tbl_menu")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Menu {
+
+    @Id
+    private Long menuCode;
+
+    @Column(name="menu_name")
+    private String menuName;
+
+    private int menuPrice;
+
+    private int categoryCode;
+
+    private String orderableStatus;
+
+    @Builder
+    public Menu(Long menuCode, String menuName, int menuPrice, int categoryCode, String orderableStatus) {
+        this.menuCode = menuCode;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.categoryCode = categoryCode;
+        this.orderableStatus = orderableStatus;
+    }
 }
